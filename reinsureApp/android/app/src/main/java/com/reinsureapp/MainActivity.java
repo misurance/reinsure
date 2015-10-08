34a2de90acf2fdb39efd27d13c4d3759da9a4381
Guy Segal <guysegal.il@gmail.com>
2015-10-08 12:26:45 +0300

    braintree

--- reinsureApp/android/app/src/main/java/com/reinsureapp/MainActivity.java
+++ reinsureApp/android/app/src/main/java/com/reinsureapp/MainActivity.java
@@ -1,17 +1,25 @@
 package com.reinsureapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
+import com.braintreepayments.api.Braintree;
+import com.braintreepayments.api.dropin.BraintreePaymentActivity;
+import com.braintreepayments.api.dropin.view.PaymentButton;
+import com.braintreepayments.api.models.PaymentMethod;
 import com.facebook.react.LifecycleState;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.paypal.android.sdk.payments.PayPalOAuthScopes;
 
-public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+import java.util.Collections;
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler, Braintree.PaymentMethodCreatedListener, Braintree.BraintreeSetupFinishedListener {
 
     private ReactInstanceManager mReactInstanceManager;
     private ReactRootView mReactRootView;
@@ -33,6 +41,16 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
         mReactRootView.startReactApplication(mReactInstanceManager, "reinsureApp", null);
 
         setContentView(mReactRootView);
+
+        //Braintree.setup(this, getIntent().getStringExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN), this);
+
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        client.get("https://your-server/client_token", new TextHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(String clientToken) {
+//                this.clientToken = clientToken;
+//            }
+//        });
     }
 
     @Override
@@ -65,5 +83,23 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
         if (mReactInstanceManager != null) {
             mReactInstanceManager.onResume(this);
         }
+    }
+
+    @Override
+    public void onBraintreeSetupFinished(boolean setupSuccessful, Braintree braintree, String errorMessage, Exception exception) {
+       String t = "";
+    }
+
+
+
+
+    @Override
+    public void onPaymentMethodCreated(PaymentMethod paymentMethod) {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int responseCode, Intent data) {
+
     }
 }
