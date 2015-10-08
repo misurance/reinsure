6785ca329c764ef15f0bbbc23a9fe4871ffbb733
Oded Welgreen <oded.welgreen@gmail.com>
2015-10-08 12:13:30 +0300

    Add button to on boarding

--- reinsureApp/OnboardingScreen.js
+++ reinsureApp/OnboardingScreen.js
@@ -8,24 +8,49 @@ var {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight
 } = React;
-
+var Button = require('react-native-button');
 
 var OnboardingScreen = React.createClass({
   render: function() {
     return (
-      <View contentContainerStyle={styles.container}>
-        <Text>
-        Onboarding Screen
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Welcome to misurance!
         </Text>
+        <Text style={styles.subHeader}>
+          Car insurance pricing is almost flat. We offer a solution for pay-as-you-go car insurance. If your car's at rest 6 days a week, you shouldn't pay as much as the guy who's driving to work every day. We're implementing real-time risk analysis algorithms, combined with hardware that''s plugged into your car, to calculate your personalized premium.
+        </Text>
+        <Button style={styles.button} onPress={this._handlePress}>
+          Connect with braintree
+        </Button>
       </View>
     );
+  },
+  _handlePress: function() {
+   console.log('button clicked');
   },
 });
 
 var styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    // backgroundColor:'yellow'
   },
+  header: {
+    justifyContent: 'center',
+    fontSize:32
+
+  },
+  subHeader: {
+    marginTop:10,
+    justifyContent: 'center',
+  },
+  button: {
+    marginTop:30
+  }
 });
 
 module.exports = OnboardingScreen;
